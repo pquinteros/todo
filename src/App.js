0226038100b3189c63ffef46f0ed325f4a22fab3
Pablo <pquinteros@gmail.com>
2019-12-21 10:04:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Header from "./components/Header/Header";
+import Input from "./components/Input/Input";
+import Tasks from "./components/Tasks/Task";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    tasks: []
+  };
+
+  onKeyPress = event => {
+    if (event.key === "Enter") {
+      // Tomo el texto del formulario
+      const text = event.target.value;
+      // Copio tasks del estado
+      if (text) {
+        const tasks = [...this.state.tasks];
+        // Agrego el texto
+        tasks.push({ title: text });
+        // Cambio el estado
+
+        this.setState({
+          tasks: tasks
+        });
+      }
+
+      // Limpia el formulario
+      event.target.value = "";
+    }
+  };
+
+  onClickAdd = () => {
+   
+      // Tomo el texto del formulario
+      const text = document.getElementById("inputAdd").value;
+      // Copio tasks del estado
+      if (text) {
+        const tasks = [...this.state.tasks];
+        // Agrego el texto
+        tasks.push({ title: text });
+        // Cambio el estado
+
+        this.setState({
+          tasks: tasks
+        });
+      }
+
+      // Limpia el formulario
+      document.getElementById("inputAdd").value = "";
+    
+  };
+
+  clickCircle = (event, index) => {
+    const tasks = [...this.state.tasks];
+
+    tasks.splice(index, 1);
+
+    this.setState({
+      tasks: tasks
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Input
+          onKeyPress={event => this.onKeyPress(event)}
+          onClickAdd={this.onClickAdd}
+        />
+        <div className="wrapTaks">
+          {this.state.tasks.map((task, index) => {
+            return (
+              <Tasks
+                title={task.title}
+                key={index}
+                clickCircle={event => this.clickCircle(event, index)}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
